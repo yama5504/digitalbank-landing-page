be614d7579bbcb1f62cc211626a2c647e31e7d52
yama <yamagaku5504@gmail.com>
2026-05-06 20:06:54 +0900

    フェードインアニメーションの追加

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import { defineConfig } from 'vite'
 // GitHub Pagesにデプロイする場合の設定
 export default defineConfig({
   base: '/digitalbank-landing-page/',
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    open: true,
+  }
 })
